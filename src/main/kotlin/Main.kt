5a31b2042cffa5215157029e637248ae6d8995e8
K_M <julius@marcel-loos.de>
2026-06-07 22:03:20 +0200

    Add YAML configuration for player acceptance tracking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import org.bukkit.Bukkit
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -10,11 +10,22 @@ import java.io.File
 
 class Main : JavaPlugin(), Listener {
 
+    private lateinit var acceptedFile: File
+    private lateinit var acceptedConfig: YamlConfiguration
+
     override fun onEnable() {
         if (!dataFolder.exists()) dataFolder.mkdirs()
 
+        // YAML Datei laden
+        acceptedFile = File(dataFolder, "accepted.yml")
+        if (!acceptedFile.exists()) {
+            acceptedFile.createNewFile()
+        }
+        acceptedConfig = YamlConfiguration.loadConfiguration(acceptedFile)
+
         server.pluginManager.registerEvents(this, this)
 
+        // Commands
         getCommand("showTOS")?.setExecutor { sender, _, _, _ ->
             if (sender is Player) showTOS(sender)
             true
@@ -35,18 +46,38 @@ class Main : JavaPlugin(), Listener {
             true
         }
 
+        getCommand("declineTOS")?.setExecutor { sender, _, _, _ ->
+            if (sender is Player) declineTOS(sender)
+            true
+        }
+
+        getCommand("declineDSE")?.setExecutor { sender, _, _, _ ->
+            if (sender is Player) declineDSE(sender)
+            true
+        }
+
         logger.info("Plugin aktiviert.")
     }
 
     override fun onDisable() {
+        acceptedConfig.save(acceptedFile)
         logger.info("Plugin deaktiviert.")
     }
 
     @EventHandler
     fun onJoin(e: PlayerJoinEvent) {
         val p = e.player
-        showTOS(p)
-        showDSE(p)
+
+        val tosAccepted = acceptedConfig.getBoolean("${p.uniqueId}.tos", false)
+        val dseAccepted = acceptedConfig.getBoolean("${p.uniqueId}.dse", false)
+
+        if (!tosAccepted) showTOS(p)
+        if (!dseAccepted) showDSE(p)
+    }
+
+    private fun saveStatus(p: Player, key: String, value: Boolean) {
+        acceptedConfig.set("${p.uniqueId}.$key", value)
+        acceptedConfig.save(acceptedFile)
     }
 
     private fun loadFile(name: String): List<String> {
@@ -83,11 +114,20 @@ class Main : JavaPlugin(), Listener {
     }
 
     fun acceptTOS(p: Player) {
+        saveStatus(p, "tos", true)
         p.sendMessage(Component.text("Du hast die TOS akzeptiert!", NamedTextColor.GREEN))
     }
 
     fun acceptDSE(p: Player) {
+        saveStatus(p, "dse", true)
         p.sendMessage(Component.text("Du hast die DSE akzeptiert!", NamedTextColor.GREEN))
     }
-}
 
+    fun declineTOS(p: Player) {
+        p.kick(Component.text("Du hast die TOS abgelehnt.", NamedTextColor.RED))
+    }
+
+    fun declineDSE(p: Player) {
+        p.kick(Component.text("Du hast die DSE abgelehnt.", NamedTextColor.RED))
+    }
+}
